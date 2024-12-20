baed70fb0226ac2f57095fb3324ec172bd8349ad
Artem-0 <artiem.ruchii.99@mail.ru>
2024-12-20 23:13:51 +0300

    Выполнил 23 задачу

---
+++ ex23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 23. Напишите программу, которая принимает на
+// вход число N и выдаёт таблицу кубов чисел от 1 до N.
+
+System.Console.WriteLine("Введите число N: ");
+int N = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+for (int i = 1; i <= N; i ++)
+{
+    System.Console.Write($"{Math.Pow(i, 3)} ");
+}
