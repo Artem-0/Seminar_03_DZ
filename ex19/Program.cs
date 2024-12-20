faf854686310ca7f237bee54591d85a63e4d435a
Artem-0 <artiem.ruchii.99@mail.ru>
2024-12-20 22:59:55 +0300

    Выполнил 19 задачу

---
+++ ex19/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 19. Напишите программу, которая принимает
+// на вход пятизначное число и проверяет, является ли
+// оно палиндромом.
+
+System.Console.WriteLine("Введите пятизначное число: ");
+string? strArray = Console.ReadLine();
+
+if (strArray?.Length == 5)
+{
+    if (strArray[0] == strArray[4] && strArray[1] == strArray[3])
+    System.Console.WriteLine("Число является Палиндромом");
+    else
+    System.Console.WriteLine("Число НЕ является Палиндромом");
+}
+else
+    System.Console.WriteLine("Введено не пятизначное число!");
